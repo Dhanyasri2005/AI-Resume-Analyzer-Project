268c29eee7152d9e4780c0a30e43251d769711fb
Dhanyasri2005 <tatadhanyasri20@gmail.com>
2026-06-06 15:18:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./config/db.js";
+// import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -10,38 +10,21 @@ import analysisRoutes from "./routes/analysisRoutes.js";
 
 dotenv.config();
 
-connectDB();
+// connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  "/api/auth",
-  authRoutes
-);
-
-app.use(
-  "/api/user",
-  userRoutes
-);
-
-app.use(
-  "/api/analysis",
-  analysisRoutes
-);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/analysis", analysisRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-app.listen(
-  process.env.PORT || 5000,
-  () => {
-    console.log(
-      `Server Running on ${process.env.PORT || 5000}`
-    );
-  }
-);
-
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server Running on ${process.env.PORT || 5000}`);
+});
